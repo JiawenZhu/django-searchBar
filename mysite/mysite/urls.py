d28a11fbfa1620f33236f58f362884ac6ae33076
Jiawen Zhu <jzhu10@mail.sfsu.edu>
2018-10-02 15:50:53 -0700

    create json file both in the data.jason and web

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('posts', include('posts.urls')),
     path('admin/', admin.site.urls),
+    
 ]
--- mysite/posts/views.py
+++ mysite/posts/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+from posts.models import Post
+from django.core import serializers
+import json
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    queryset = Post.objects.all()
+    context = {
+        "title": "Project list",
+        "objects": queryset
+    }
+    serialized_queryse = serializers.serialize('json', queryset)
+    with open('data.json', 'w') as outfile:
+        json.dump(serialized_queryse, outfile)
+    return HttpResponse(serialized_queryse, content_type='application/json')
 
 
