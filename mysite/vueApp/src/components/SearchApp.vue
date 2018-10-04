786f0abb7570b5f953e9975ac95069959f546cfc
Jiawen Zhu <jzhu10@mail.sfsu.edu>
2018-10-03 23:54:04 -0700

    message

--- mysite/vueApp/src/components/SearchApp.vue
+++ mysite/vueApp/src/components/SearchApp.vue
@@ -1,23 +1,43 @@
 <template>
   <div id='SearchApp'>
     <input type="text" v-model="search" placeholder="search a user"/>
-    <!-- <div v-for="x in data"> -->
-      <h3>{{name}}</h3>
-    <!-- </div> -->
+    <div v-for="x in data">
+      <h3>title: {{x.fields.title}}</h3>
+      <h3>description: {{x.fields.description}}</h3>
+      <h3>price: {{x.fields.price}}</h3>
+      <h3>summary: {{x.fields.summary}}</h3>
+      <h3>timestamp: {{x.fields.timestamp}}</h3>
+      <br/><br/>
+    </div>
 
   </div>
 </template>
 <script>
 import json from '../../../data.json';
-var newData = JSON.parse(json);
+const project = JSON.parse(json);
+
 export default {
   name: 'SearchApp',
   data () {
     return {
-      name: newData,
+      // title: '',
+      // description: '',
+      // price: '',
+      // summary: '',
+      // timestamp: '',
+      data: project
     }
+  },
+  method: {
+      title: function(){
+        return 'title: ';
+        // Object.keys(this.newData).forEach(function(key){
+          // var value = obj[key];
+        // });
+      }
   }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
